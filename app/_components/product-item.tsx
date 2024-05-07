7e3c5a9c57f293594716abb49a36b1bb464f61cd
David Ribeiro <david.ribeiro@solidaridadnetwork.org>
2024-05-07 00:40:06 -0300

    feat: format price

---
+++ app/_components/product-item.tsx
@@ -0,0 +1,45 @@
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
+import { ArrowDown, ArrowDownIcon } from "lucide-react";
+
+interface ProductItemProps {
+  product: Product;
+}
+const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <div className="space-y-2 w-[150px] min-w-[150px]">
+      <div className="relative h-[150px] w-full">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          fill
+          className="object-cover rounded-lg shadow-md"
+        />
+        {product.discountPercentage > 0 && (
+          <div className="absolute top-2 left-2 flex items-center bg-primary text-white px-2 py-1 rounded-full">
+            <ArrowDownIcon size={16} />
+            <span className=" text-xs font-bold">
+              {product.discountPercentage}%
+            </span>
+          </div>
+        )}
+      </div>
+      <div>
+        <h2 className="text-sm truncate">{product.name}</h2>
+      </div>
+      <div className="flex gap-1 items-center">
+        <h3 className="font-bold">
+          {formatCurrency(calculateProductTotalPrice(product))}
+        </h3>
+        {product.discountPercentage > 0 && (
+          <span className="line-through text-muted-foreground text-sm">
+            {formatCurrency(Number(product.price))}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
---
+++ app/_components/product-list.tsx
@@ -0,0 +1,22 @@
+import { db } from "../_lib/prisma";
+import ProductItem from "./product-item";
+
+const ProductList = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+  });
+  return (
+    <div className="flex overflow-x-scroll gap-4 px-5">
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
